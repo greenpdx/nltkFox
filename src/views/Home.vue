f37d3ba4729aedb2cf3787837e68db9e4ec3940c
Shaun Savage <savages@taxnvote.org>
2020-07-27 19:49:17 +0000

    start to add GUI

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <!--img alt="Vue logo" src="../assets/logo.png"-->
+    <Main :msg="msg"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Main from '@/components/Main.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Main
+  },
+  data () {
+    return {
+      msg: 'Analyze FoxNews'
+    }
+  }
+}
+</script>
